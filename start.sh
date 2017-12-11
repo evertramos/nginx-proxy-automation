19c6e45683a98764c504947aee52d23ac28ae715
Evert Ramos <evert.ramos@gmail.com>
2017-12-11 08:57:00 -0500

    Fix standard file names

---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# This file should be used to prepare and run your WebProxy after set up your .env file
+# Source: https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion
+#
+
+# 1. Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else 
+    echo "Please set up your .env file before starting your enviornment."
+    exit 1
+fi
+
+# 2. Create docker network
+docker network create $NETWORK
+
+# 3. Verify if second network is configured
+if [ ! -z ${SERVICE_NETWORK+X} ]; then
+    docker network create $SERVICE_NETWORK
+fi
+
+# 4. Download the latest version of nginx.tmpl
+curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
+
+# 5. Start proxy
+
+# Check if you have multiple network
+if [ -z ${SERVICE_NETWORK+X} ]; then
+    docker-compose up -d
+else
+    docker-compose -f docker-compose-multiple-networks.yml up -d
+fi
+
+exit 0
---
+++ test_stop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Stop and remove test enviornment
+docker stop test-web && docker rm test-web 
+
+exit 0
