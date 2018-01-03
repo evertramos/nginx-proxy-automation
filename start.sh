172e55c59f8e2e1f79d3a2e4892c8392c169a089
Evert Ramos <evert.ramos@gmail.com>
2018-01-03 13:33:36 -0500

    Add conf.d options

--- start.sh
+++ start.sh
@@ -27,7 +27,36 @@ curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > n
 # 5. Update local images
 docker-compose pull
 
-# 6. Start proxy
+# 6. Add any special configuration if it's set in .env file
+
+# Check if user set to use Special Conf Files
+if [ ! -z ${USE_NGINX_CONF_FILES+X} ] && [ "$USE_NGINX_CONF_FILES" = true ]; then
+
+    # Create the conf folder if it does not exists
+    mkdir -p $NGINX_FILES_PATH/conf.d
+
+    # Copy the special configurations to the nginx conf folder
+    cp -R ./conf.d/* $NGINX_FILES_PATH/conf.d
+
+    # Check if there was an error and try with sudo
+    if [ $? -ne 0 ]; then
+        sudo cp -R ./conf.d/* $NGINX_FILES_PATH/conf.d
+    fi
+
+    # If there was any errors inform the user
+    if [ $? -ne 0 ]; then
+        echo
+        echo "#######################################################"
+        echo
+        echo "There was an error trying to copy the nginx conf files."
+        echo "The webproxy will still work, your custom configuration"
+        echo "will not be loaded."
+        echo 
+        echo "#######################################################"
+    fi
+fi 
+
+# 7. Start proxy
 
 # Check if you have multiple network
 if [ -z ${SERVICE_NETWORK+X} ]; then
