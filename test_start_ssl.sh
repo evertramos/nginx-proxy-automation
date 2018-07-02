07830cb94ae91d8c47cc42cf1f7c867ed9f29e84
Evert Ramos <evertramos@users.noreply.github.com>
2018-07-02 09:53:42 -0300

    fix typo

--- test_start_ssl.sh
+++ test_start_ssl.sh
@@ -6,7 +6,7 @@ NAME=test-web
 # Set up your DOMAIN
 if [ $# -eq 0 ]; then
     echo "Please inform your domain name to test your proxy."
-    echo "./test_start.sh $1"
+    echo "./test_start_ssl.sh $1"
     exit 1
 else
     DOMAIN=$1
