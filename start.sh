d90a97926f6f8ac9b4384ea29f8fc1f201775b74
iabotsiTC <33929623+iabotsiTC@users.noreply.github.com>
2018-08-09 17:29:17 +0200

    Update start.sh
    Fixing a small typo

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@
 if [ -e .env ]; then
     source .env
 else 
-    echo "Please set up your .env file before starting your enviornment."
+    echo "Please set up your .env file before starting your environment."
     exit 1
 fi
 
